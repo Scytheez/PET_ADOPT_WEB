1dd2a541b8ce0d569e0396fbe80f9b63a7be7343
XuX <floresvittorio@gmail.com>
2024-05-06 01:07:02 +0800

    db patch

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
 from flask import Flask, render_template, url_for, request
-from flask_sqlalchemy import SQLAlchemy
 from dbconfig import conn
 
 db_conn = conn()
 
 app =Flask(__name__)
-
-#app.config['SQLALCHEMY_DATABASE_URI'] = ''
 
 @app.route('/')
 def index():
@@ -23,31 +20,34 @@ def ouranimals():
 @app.route('/rehome', methods=['GET', 'POST'])  
 def rehome():
     if request.method == 'POST':
-        # Get form pet data
-        pet_name = request.form['name']
-        pet_age = request.form['age']
-        gender = request.form['gender']
-        breed = request.form['breed']
-        ownership = request.form['ownership']
-        type = request.form['type']
-        reasons = request.form['reasons']
-        desc_pet = request.form['describe']
-        vaccine = request.form['Vaccinated']
-        vaccine1 = request.form['vaccinated1']
-        pet_pic = request.form['file']
+        try:
+            # Get form pet data
+            pet_name = request.form['name']
+            pet_age = request.form['age']
+            gender = request.form['gender']
+            breed = request.form['breed']
+            ownership = request.form['ownership']
+            type = request.form['type']
+            reasons = request.form['reasons']
+            desc_pet = request.form['describe']
+            vaccine = request.form['Vaccinated']
+            vaccine1 = request.form['vaccinated1']
+            pet_pic = request.form['file']
 
-        # Get form user data
-        name = request.form['name']
-        age = request.form['age']
-        email = request.form['email']
-        occupation = request.form['occupation']
-        soc_media = request.form['social']
-        phone_num = request.form['phone']
-        valid_id = request.form['image']
+            # Get form user data
+            name = request.form['name']
+            age = request.form['age']
+            email = request.form['email']
+            occupation = request.form['occupation']
+            soc_media = request.form['social']
+            phone_num = request.form['phone']
+            valid_id = request.form['image']
 
-        db_conn.rehome_form(pet_name, pet_age, gender, breed, ownership, type, 
-                            reasons, desc_pet, vaccine, vaccine1, pet_pic, name,
-                            age, email, occupation, soc_media, phone_num, valid_id)
+            db_conn.rehome_form(pet_name, pet_age, gender, breed, ownership, type, 
+                                reasons, desc_pet, vaccine, vaccine1, pet_pic, name,
+                                age, email, occupation, soc_media, phone_num, valid_id)
+        except Exception as e:
+            print(e)
 
     return render_template('rehome.html')
 
@@ -58,34 +58,39 @@ def donation():
 @app.route('/adopt')  
 def adopt():
     if request.method == 'POST':
-        # Get form applicant data
-        name = request.form['Fname']
-        lname = request.form['Lname']
-        addr = request.form['address']
-        phone = request.form['phone']
-        email = request.form['email']
-        occu = request.form['occupation']
-        social = request.form['social']
-        bday = request.form['bday']
+        try:
+            # Get form applicant data
+            name = request.form['Fname']
+            lname = request.form['Lname']
+            addr = request.form['address']
+            phone = request.form['phone']
+            email = request.form['email']
+            occu = request.form['occupation']
+            social = request.form['social']
+            bday = request.form['bday']
 
-        f_pet = request.form['f_pet']
-        living_type = request.form['building']
-        allergy = request.form['allergy']
-        what_pet = request.form['what_pet']
-        live = request.form['live']
+            f_pet = request.form['f_pet']
+            living_type = request.form['building']
+            allergy = request.form['allergy']
+            what_pet = request.form['what_pet']
+            live = request.form['live']
 
-        pet_resp = request.form['pet_resp']
-        pet_needs = request.form['pet_needs']
-        pet_emer = request.form['pet_emer']
-        pet_hour = request.form['pet_hour']
-        pet_env = request.form['pet_env']
-        pet_supp = request.form['pet_supp']
-        no_supp = request.form['no_supp']
-        other_pet = request.form['other_pet']
-        past_pet = request.form['past_pet']
-        valid_id = request.form['file']
+            pet_resp = request.form['pet_resp']
+            pet_needs = request.form['pet_needs']
+            pet_emer = request.form['pet_emer']
+            pet_hour = request.form['pet_hour']
+            pet_env = request.form['pet_env']
+            pet_supp = request.form['pet_supp']
+            no_supp = request.form['no_supp']
+            other_pet = request.form['other_pet']
+            past_pet = request.form['past_pet']
+            valid_id = request.form['file']
 
-        db_conn.adopt_form()
+            db_conn.adopt_form(name, lname, addr, phone, email, occu, social, bday, f_pet, living_type,
+                            allergy, what_pet, live, pet_resp, pet_needs, pet_emer, pet_hour, pet_env,
+                            pet_supp, no_supp, other_pet, past_pet, valid_id)
+        except Exception as e:
+            print(e)
 
     return render_template('adopt.html')
 
--- dbconfig.py
+++ dbconfig.py
@@ -15,12 +15,21 @@ class conn:
 
         self.cursor = self.db_conn.cursor()
 
-    def adopt_form(self):
-        pass
+    def adopt_form(self, name, lname, addr, phone, email, occu, social, bday, f_pet, living_type,
+                    allergy, what_pet, live, pet_resp, pet_needs, pet_emer, pet_hour, pet_env,
+                    pet_supp, no_supp, other_pet, past_pet, valid_id):
+        user_qry = "INSERT INTO USER(name, lname, address, phone_number, email, occupation, social_media_profile, bday, valid_id, user_type) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        self.cursor.execute(user_qry, (name, lname, addr, phone, email, occu, social, bday, valid_id, 'adopt'))
+
+        pet_qry = 'INSERT INTO ADOPT_PET(first_pet, living_type, allergic, what_pet, live_with, pet_health, pet_needs, pet_emergency, pet_alone, pet_env, fam_supp, if_no_explain, other_pet, past_pet) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
+        self.cursor.execute(pet_qry, (f_pet, living_type, allergy, what_pet, live, pet_resp, pet_needs, pet_emer, pet_hour, pet_env, pet_supp, no_supp, other_pet, past_pet))
+
+        self.db_conn.commit()
+        self.cursor.close()
 
     def rehome_form(self, pet_name, pet_age, gender, breed, ownership, type, 
-                            reasons, desc_pet, vaccine, vaccine1, pet_pic, name,
-                            age, email, occupation, soc_media, phone_num, valid_id):
+                    reasons, desc_pet, vaccine, vaccine1, pet_pic, name,
+                    age, email, occupation, soc_media, phone_num, valid_id):
         pet_qry = 'INSERT INTO PET(pet_name, pet_age, pet_gender, pet_breed, pet_type, description, vaccinated, vaccine_type, picture, pet_status) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
         self.cursor.execute(pet_qry, (pet_name, pet_age, gender, breed, type, desc_pet, vaccine, vaccine1, pet_pic, 'available'))
 
